7b976c8253963794c1bbf938c6e85cff9f3b3e77
Lars Tingelstad <lars.tingelstad@ntnu.no>
2021-11-17 15:12:06 +0100

    specify arm controller name

--- kuka_kr6r900sixx_moveit_config/launch/demo.launch.py
+++ kuka_kr6r900sixx_moveit_config/launch/demo.launch.py
@@ -186,7 +186,7 @@ def generate_launch_description():
 
     # Load controllers
     load_controllers = []
-    for controller in ["arm_controller", "joint_state_broadcaster"]:
+    for controller in ["kr6r900sixx_arm_controller", "joint_state_broadcaster"]:
         load_controllers += [
             ExecuteProcess(
                 cmd=["ros2 run controller_manager spawner.py {}".format(controller)],
