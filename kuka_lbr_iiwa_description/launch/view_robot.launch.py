f88b3f42fdc28219b2c05207978e9af93648af39
Lars Tingelstad <lars.tingelstad@ntnu.no>
2022-02-14 14:47:26 +0100

    Added iiwa

---
+++ kuka_lbr_iiwa_description/launch/view_robot.launch.py
@@ -0,0 +1,57 @@
+from launch import LaunchDescription
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    PathJoinSubstitution,
+)
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("kuka_lbr_iiwa_support"),
+                    "urdf",
+                    "lbr_iiwa_14_r820.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("kuka_lbr_iiwa_support"), "config", "view_robot.rviz"]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    nodes = [joint_state_publisher_gui_node, robot_state_pub_node, rviz_node]
+
+    return LaunchDescription(nodes)
